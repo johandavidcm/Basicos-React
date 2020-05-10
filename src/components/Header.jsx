bb775d5850dba6a0c5500f2e42972a81a389e6d5
Johan David <johancarras16@gmail.com>
2020-05-10 18:03:00 -0500

    Creacion del segundo componente

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
 function Header(){
+    const edad = 28;
+    let mensaje;
+
+    if(edad>=18){
+        mensaje = 'eres mayor de edad';
+    } else {
+        mensaje = 'Eres menor de edad';
+    }
+
     return(
-        <h1>Desde el Header</h1>
+        <h1 className="encabezado">Tienda Virtual</h1>
     );
 }
 
